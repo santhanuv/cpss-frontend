0d287e6091231cd9e84ea778f05be4a778ac0833
santhanuv17 <santhanuv17@gmail.com>
2022-06-29 21:03:39 +0530

    Refactor Code

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import GlobalStyles from "./components/GlobalStyles";
 import theme from "./theme";
 import { ThemeProvider } from "@emotion/react";
 import Login from "./pages/Login/Index";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./pages/UserDashboard";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
         <GlobalStyles />
-        <Login />
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
       </div>
     </ThemeProvider>
   );
